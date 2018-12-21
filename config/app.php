7a637810a9f9b7e723ff16e6a82f0edc14b71f7b
heropoo <aiyouyou1000@163.com>
2018-12-21 15:01:57 +0800

    config

--- config/app.php
+++ config/app.php
@@ -9,20 +9,34 @@
 return [
     'title' => 'Deployer',    //部署系统标题
 
-    'deployer_log_file' => dirname(__DIR__) . '/runtime/logs/deployer-' . date('Y-m-d') . '.log',    //日志路径
+    'deployer_log_file' => dirname(__DIR__) . '/runtime/logs/deployer-deployer-' . date('Y-m-d') . '.log',    //日志路径
+    'server_log_file' => dirname(__DIR__) . '/runtime/logs/deployer-server-' . date('Y-m-d') . '.log',    //日志路径
 
     'secret_key' => 'W4sxjDXyLcnoNWauDH3pI0nrdSdmYUKL',       //加密密钥 上线请修改！！
-
-    'users' => require __DIR__ . '/users.php', //管理员账号
 
     'projects' => [   //发布项目列表
         'project1' => '示例项目1',
         'project2' => '示例项目2',
     ],
 
+    'project_paths' => [   //项目在目标机器上的路径
+        'project1' => '/var/www/project1',
+        'project2' => '/var/www/project2',
+    ],
+
     'servers' => [    //目标机器列表
         'prod-1' => 'http://192.168.31.4:8081',  //如果部署机器和目标机器在同一内网，建议使用内网地址
         'prod-2' => 'http://192.168.31.5:8081',
         'prod-3' => 'http://192.168.31.6:8081'
-    ]
+    ],
+
+    'server_users' => [    //目标机器上执行用来git命令的用户
+        'prod-1' => 'root',
+        'prod-2' => 'www-data',
+        'prod-3' => 'www-data'
+    ],
+
+    'current_server'=>'prod-1', //在目标机器部署代码时，指定下当前机器
+
+    'users' => require __DIR__ . '/users.php', //管理员账号
 ];
--- server/index.php
+++ server/index.php
@@ -2,6 +2,118 @@
 /**
  * Created by PhpStorm.
  * User: ttt
- * Date: 2018/12/21
- * Time: 11:43
+ * Date: 2018/7/23
+ * Time: 10:58
  */
+
+//ini_set('display_errors', 'On');
+//error_reporting(E_ALL);
+
+$config = require dirname(__DIR__).'/config/app.php';
+
+date_default_timezone_set('Asia/Shanghai');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $token = isset($_POST['token']) ? trim($_POST['token']) : '';
+    if ($token !== md5($config['secret_key'].date('Y-m-d H'))) {
+        return_json(1, 'error token');
+    }
+
+    $action = isset($_POST['action']) ? trim($_POST['action']) : '';
+    if (empty($action)) {
+        return_json(1, 'empty action');
+    }
+
+    $project = isset($_POST['project']) ? trim($_POST['project']) : '';
+    if (empty($project)) {
+        return_json(1, 'empty project');
+    }
+
+    if ($action == 'status') {    // git status
+        $res = git_status($project);
+        return_json(0, 'git status', $res);
+    } else if ($action == 'checkout') {    //git checkout tag_name
+        $tag = isset($_POST['tag']) ? trim($_POST['tag']) : '';
+        if (empty($tag)) {
+            return_json(1, 'empty tag');
+        }
+        $res = git_checkout($tag, $project);
+        return_json(0, 'git checkout ' . $tag, $res);
+    }
+}
+
+function git_status($project)
+{
+    return git_execute(["git status"], $project);
+}
+
+function git_checkout($tag, $project)
+{
+    return git_execute(["git fetch", "git checkout $tag"], $project);
+}
+
+function git_execute(array $cmds, $project)
+{
+    global $config;
+
+    $current_server = $config['current_server'];
+    $current_server_user = $config['server_users'][$current_server];
+    if($current_server_user == 'root'){
+        $cmd = implode(' && ', $cmds);
+    }else{
+        $cmd = '';
+        foreach ($cmds as $cmd_item){
+            $cmd .= "&& sudo -u $current_server_user ".$cmd_item;
+        }
+        $cmd = substr($cmd, 3);
+    }
+
+    $path = $config['project_paths'][$project];
+    $log_file = $config['server_log_file'];
+
+    error_log("[".date('Y-m-d H:i:s')."] Deploy execute `$cmd` in `$path`", 3, $log_file);
+
+    $descriptorspec = array(
+        0 => array("pipe", "r"),  // 标准输入，子进程从此管道中读取数据
+        1 => array("pipe", "w"),  // 标准输出，子进程向此管道中写入数据
+        2 => array("pipe", "w") // 标准错误
+    );
+
+    $cwd = $path;
+
+    $process = proc_open($cmd, $descriptorspec, $pipes, $cwd);
+
+    if (is_resource($process)) {
+        $success_msg = stream_get_contents($pipes[1]);
+        fclose($pipes[1]);
+
+        $error_msg = stream_get_contents($pipes[2]);
+        fclose($pipes[2]);
+
+        $return_value = proc_close($process);
+
+        //echo PHP_EOL."Command returned $return_value\n";
+
+        if ($return_value === 0) {
+            //echo PHP_EOL."Command returned success.";
+            error_log("Command returned success." . PHP_EOL, 3, $log_file);
+        } else {
+            //echo PHP_EOL."Command returned failed ".$return_value;
+            error_log("Command returned failed " . $return_value . ' ' . $error_msg . PHP_EOL, 3, $log_file);
+        }
+        return [
+            'return_value' => $return_value,
+            'success_msg' => $success_msg,
+            'error_msg' => $error_msg
+        ];
+    }
+    return [];
+}
+
+function return_json($code, $msg, $data = [])
+{
+    header('Content-type: application/json;charset=utf-8');
+    echo json_encode(['code' => $code, 'msg' => $msg, 'data' => $data]);
+    die();
+}
