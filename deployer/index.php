a653281db50eac388b9bfe16fa662621de96114c
heropoo <aiyouyou1000@163.com>
2022-04-11 00:56:03 +0800

    page

--- deployer/index.php
+++ deployer/index.php
@@ -38,55 +38,91 @@ $dst_project = isset($_POST['project']) ? trim($_POST['project']) : '';
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- 引入 Bootstrap -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <!-- HTML5 Shiv 和 Respond.js 用于让 IE8 支持 HTML5元素和媒体查询 -->
+    <!-- 注意： 如果通过 file://  引入 Respond.js 文件，则该文件无法起效果 -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
     <title><?= $config['title'] ?></title>
     <style>
-        pre {
-            overflow-x: auto;
-            width: 600px;
-            background-color: #efefef;
-            padding: 5px 10px;
+        .main{
+            padding: 2rem 4rem;
         }
+        /*pre {*/
+        /*    overflow-x: auto;*/
+        /*    width: 600px;*/
+        /*    background-color: #efefef;*/
+        /*    padding: 5px 10px;*/
+        /*}*/
     </style>
 </head>
 <body>
-<div>
-    服务器查询状态
-    <form action="" method="post" style="padding: 10px">
-        <input type="hidden" name="action" value="status">
-        项目：<select name="project">
-            <?php foreach ($projects as $project => $project_name): ?>
-                <option value="<?= $project ?>"
-                        <?php if ($dst_project === $project): ?>selected<?php endif; ?>><?= $project_name ?></option>
-            <?php endforeach; ?>
-        </select>
-        <button type="submit">查询</button>
-    </form>
-</div>
-<hr>
-代码发布
-<form action="" method="post" style="padding: 10px">
-    版本:<input type="text" name="tag" value="<?= $tag ?>" placeholder="请输入tag" required>
-    项目:<select name="project">
-        <?php foreach ($projects as $project => $project_name): ?>
-            <option value="<?= $project ?>"
-                    <?php if ($dst_project === $project): ?>selected<?php endif; ?>><?= $project_name ?></option>
-        <?php endforeach; ?>
-    </select>
+<div class="main container-fluid">
+    <div class="row">
+        <h2>服务器查询状态</h2>
+        <form action="" method="post" class="form form-inline">
+            <input type="hidden" name="action" value="status">
+            <div class="form-group">
+                <label for="">项目:</label>
+                <select name="project" class="form-control" style="width: 20rem">
+                    <?php foreach ($projects as $project => $project_name): ?>
+                        <option value="<?= $project ?>"
+                                <?php if ($dst_project === $project): ?>selected<?php endif; ?>><?= $project_name ?></option>
+                    <?php endforeach; ?>
+                </select>
+                <button type="submit" class="btn btn-info">查询</button>
+            </div>
+        </form>
+    </div>
 
-    机器:<select name="host">
-        <option value="">全部</option>
-        <?php foreach ($hosts as $host => $url): ?>
-            <option value="<?= $host ?>" <?php if ($dst_host === $host): ?>selected<?php endif; ?>><?= $host ?></option>
-        <?php endforeach; ?>
-    </select>
-    <input type="hidden" name="username" value="$username">
-    <input type="hidden" name="action" value="checkout">
-    <button type="submit">发布</button>
-</form>
-<hr>
+
+    <hr>
+
+    <div class="row">
+        <h2>代码发布</h2>
+        <form action="" method="post" class="form form-inline">
+            <div class="form-group">
+                <label for="">版本:</label>
+                <input class="form-control" type="text" name="tag" value="<?= $tag ?>" placeholder="请输入tag" required>
+            </div>
+            <div class="form-group">
+                <label for="">项目:</label>
+                <select name="project" class="form-control">
+                    <?php foreach ($projects as $project => $project_name): ?>
+                        <option value="<?= $project ?>"
+                                <?php if ($dst_project === $project): ?>selected<?php endif; ?>><?= $project_name ?></option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="">机器:</label>
+                <select name="host" class="form-control">
+                    <option value="">全部</option>
+                    <?php foreach ($hosts as $host => $url): ?>
+                        <option value="<?= $host ?>" <?php if ($dst_host === $host): ?>selected<?php endif; ?>><?= $host ?></option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for=""></label>
+                <button type="submit" class="btn btn-primary">发布</button>
+            </div>
+
+            <input type="hidden" name="action" value="checkout">
+
+        </form>
+    </div>
+
+    <hr>
+
+    <div class="row">
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -124,10 +160,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 }
 
-echo '
-<p style="text-align:center;position:fixed;bottom:1rem;left:1rem;">&copy; 2018 by <a href="https://github.com/heropoo/deployer">Heropoo Deployer</a></p>
-</body></html>';
-
 function sub_curl($url, $data)
 {
     $ch = curl_init();
@@ -142,3 +174,13 @@ function sub_curl($url, $data)
     curl_close($ch);
     return $data;
 }
+?>
+    </div>
+</div>
+<p style="text-align:center;position:fixed;bottom:1rem;left:1rem;">&copy; 2018 - <?= date('Y')?> <a href="https://github.com/heropoo/deployer">Deployer</a></p>
+<!-- jQuery (Bootstrap 的 JavaScript 插件需要引入 jQuery) -->
+<script src="https://code.jquery.com/jquery.js"></script>
+<!-- 包括所有已编译的插件 -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
