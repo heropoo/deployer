cda5061d6d763189b980a0bb5a04f8999ea02cd5
heropoo <aiyouyou1000@163.com>
2022-06-23 01:20:12 +0800

    test

---
+++ docs/sqlite/up.sql
@@ -0,0 +1,29 @@
+CREATE TABLE user(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username VARCHAR(50) NOT NULL,
+  password CHAR(50) not NULL,
+  create_time timestamp NOT NULL DEFAULT (datetime('now', 'localtime')),
+  update_time datetime NOT NULL DEFAULT (datetime('now', 'localtime'))
+);
+CREATE INDEX idx_username ON user(username);
+
+CREATE TABLE host(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(50) NOT NULL,
+  host VARCHAR(50) not NULL,
+  port SMALLINT not NULL,
+  user VARCHAR(50) not NULL,
+  create_time timestamp NOT NULL DEFAULT (datetime('now', 'localtime')),
+  update_time datetime NOT NULL DEFAULT (datetime('now', 'localtime'))
+);
+
+CREATE TABLE project(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(50) NOT NULL,
+  path VARCHAR(255) not NULL,
+  branch VARCHAR(50) not NULL,
+  hosts VARCHAR(255) not NULL, -- 多个host id 逗号分隔
+  create_time timestamp NOT NULL DEFAULT (datetime('now', 'localtime')),
+  update_time datetime NOT NULL DEFAULT (datetime('now', 'localtime'))
+);
+
