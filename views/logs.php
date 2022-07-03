147b936d1f26b13c416366fdc133a3f02b6758bc
heropoo <aiyouyou1000@163.com>
2022-07-04 00:23:10 +0800

    logs

--- views/logs.php
+++ views/logs.php
@@ -1,9 +1,33 @@
-
+<style>
+    .logs-list{
+        margin-bottom: 5rem;
+    }
+    .log-item{
+        padding: 1rem;
+        margin-bottom: 1rem;
+    }
+</style>
 <div class="container-fluid">
     <div class="row">
         <ul class="logs-list">
     <?php foreach ($lines as $line):?>
-        <li><?= $line?></li>
+    <?php $line = json_decode($line, 1);?>
+        <div class="log-item bg-success">
+
+            <div>时间: <?= $line['time']?></div>
+            <div>项目: <?= $line['project']?></div>
+            <div>用户: <?= $line['user']?></div>
+            <div>结果:
+                <ul>
+                <?php foreach ($line['data'] as $item):?>
+                    <li>
+                        <div><?= $item['msg']?> <?= $item['code'] === 0 ? '✔️ Success' : '❌ Failed'?></div>
+                    </li>
+                <?php endforeach;?>
+                </ul>
+            </div>
+
+        </div>
     <?php endforeach;?>
         </ul>
     </div>
