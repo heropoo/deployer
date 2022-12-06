b5136638db9d039e268ac025810fca4f1bdd3627
heropoo <aiyouyou1000@163.com>
2022-12-07 01:08:45 +0800

    v1.6.4-preview

--- public/index.php
+++ public/index.php
@@ -10,5 +10,5 @@ require __DIR__.'/../vendor/autoload.php';
 use Moon\Application;
 
 $app = new Application(dirname(__DIR__));
-APP::setVersion('v1.6.3');
+APP::setVersion('v1.6.4-preview');
 $app->run();
