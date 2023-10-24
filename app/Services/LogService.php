65be557ae173a1a9ed794f62151533332eb9cda3
heropoo <aiyouyou1000@163.com>
2023-10-24 18:25:23 +0800

    log

--- app/Services/LogService.php
+++ app/Services/LogService.php
@@ -15,34 +15,33 @@ class LogService
             return false;
         }
         if (file_exists($log_file)) {
-            ftruncatestart($log_file, 1 * 1024 * 1024); // 1MB
+            self::ftruncatestart($log_file, 1 * 1024 * 1024); // 1MB
         }
     }
 
-
-}
-
-/**
- * @param $filename
- * @param $maxfilesize
- * @see https://www.php.net/manual/zh/function.ftruncate.php#103591
- */
-function ftruncatestart($filename, $maxfilesize)
-{
-    $size = filesize($filename);
-    if ($size < $maxfilesize * 1.0) return;
-    $maxfilesize = $maxfilesize * 0.5; //we don't want to do it too often...
-    $fh = fopen($filename, "r+");
-    $start = ftell($fh);
-    fseek($fh, -$maxfilesize, SEEK_END);
-    $drop = fgets($fh);
-    $offset = ftell($fh);
-    for ($x = 0; $x < $maxfilesize; $x++) {
-        fseek($fh, $x + $offset);
-        $c = fgetc($fh);
-        fseek($fh, $x);
-        fwrite($fh, $c);
+    /**
+     * @param $filename
+     * @param $maxfilesize
+     * @see https://www.php.net/manual/zh/function.ftruncate.php#103591
+     */
+    private static function fTruncateStart($filename, $maxfilesize)
+    {
+        $size = filesize($filename);
+        if ($size < $maxfilesize * 1.0) return;
+        $maxfilesize = $maxfilesize * 0.5; //we don't want to do it too often...
+        $fh = fopen($filename, "r+");
+        $start = ftell($fh);
+        fseek($fh, -$maxfilesize, SEEK_END);
+        $drop = fgets($fh);
+        $offset = ftell($fh);
+        for ($x = 0; $x < $maxfilesize; $x++) {
+            fseek($fh, $x + $offset);
+            $c = fgetc($fh);
+            fseek($fh, $x);
+            fwrite($fh, $c);
+        }
+        ftruncate($fh, $maxfilesize - strlen($drop));
+        fclose($fh);
     }
-    ftruncate($fh, $maxfilesize - strlen($drop));
-    fclose($fh);
 }
+
