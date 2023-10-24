8788831607df615c774a9c129f48b8e6b194f991
heropoo <aiyouyou1000@163.com>
2023-10-24 18:18:53 +0800

    log

--- app/Services/LogService.php
+++ app/Services/LogService.php
@@ -9,19 +9,40 @@ namespace App\Services;
 
 class LogService
 {
-    public static function gc($log_file){
-        //todo 日志滚动
-        return;
-        if(mt_rand(0, 10) != 10){
-            //return false;
+    public static function gc($log_file)
+    {
+        if (mt_rand(0, 10) != 10) {
+            return false;
         }
         if (file_exists($log_file)) {
-            $lines = file($log_file);
-            $lines_count = count($lines);
-            $cut_count = $lines_count - 10;
-            if($cut_count > 0){
-
-            }
+            ftruncatestart($log_file, 1 * 1024 * 1024); // 1MB
         }
     }
+
+
+}
+
+/**
+ * @param $filename
+ * @param $maxfilesize
+ * @see https://www.php.net/manual/zh/function.ftruncate.php#103591
+ */
+function ftruncatestart($filename, $maxfilesize)
+{
+    $size = filesize($filename);
+    if ($size < $maxfilesize * 1.0) return;
+    $maxfilesize = $maxfilesize * 0.5; //we don't want to do it too often...
+    $fh = fopen($filename, "r+");
+    $start = ftell($fh);
+    fseek($fh, -$maxfilesize, SEEK_END);
+    $drop = fgets($fh);
+    $offset = ftell($fh);
+    for ($x = 0; $x < $maxfilesize; $x++) {
+        fseek($fh, $x + $offset);
+        $c = fgetc($fh);
+        fseek($fh, $x);
+        fwrite($fh, $c);
+    }
+    ftruncate($fh, $maxfilesize - strlen($drop));
+    fclose($fh);
 }
