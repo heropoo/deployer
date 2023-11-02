4d70796f24da083c9d53fa008fb36257f0da6f4c
heropoo <aiyouyou1000@163.com>
2023-11-02 18:01:11 +0800

    hosts

--- config/hosts.php
+++ config/hosts.php
@@ -4,17 +4,4 @@
  * Datetime: 2022/6/12 8:27 下午
  */
 
-return [
-    'prod-1' => [
-        'name' => 'prod-1',
-        'host' => '192.168.1.3',
-        'port' => 22,
-        'user' => 'root',
-    ],
-    'prod-2' => [
-        'name' => 'prod-2',
-        'host' => '192.168.1.4',
-        'port' => 22,
-        'user' => 'root',
-    ],
-];
+return [];
